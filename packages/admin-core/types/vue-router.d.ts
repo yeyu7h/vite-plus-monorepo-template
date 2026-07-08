a9e3f2300249369f58393abe54b78cef1b07b2b2
yeyuqh <zhaoxw1028@gmail.com>
2026-07-08 09:14:14 +0800

    chore: 移除不再使用的 @typescript/native-preview 依赖和相关文件

--- packages/admin-core/types/vue-router.d.ts
+++
@@ -1,8 +0,0 @@
-import type { AdminRouteMeta } from './src/app.d.ts'
-
-import 'vue-router'
-
-declare module 'vue-router' {
-  // oxlint-disable-next-line typescript/no-empty-object-type
-  interface RouteMeta extends AdminRouteMeta {}
-}
